171b2c816314c183913cfa999921fb5bac1bd701
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-02-13 14:00:44 +0700

    percobaan 2

---
+++ MINGGU1/perulangan.java
@@ -0,0 +1,35 @@
+package MINGGU1;
+
+import java.util.Scanner;
+public class perulangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     
+        System.out.println("Masukkan NIM: ");
+        String nim = sc.nextLine();
+        
+        //mengambil 2 digit terakhir
+        int n = Integer.parseInt(nim.substring(nim.length() - 2));
+        
+        if (n < 10) {
+            n += 10;
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i == 10 || i == 15) {
+                continue;
+            }
+
+            if ( i % 3 == 0) {
+                System.out.print("# ");
+            } else if ( i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
+                System.out.print("* ");
+            }
+        }
+        sc.close();
+    }
+}
+
