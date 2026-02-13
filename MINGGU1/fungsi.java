cf17235399fbc14d7a75ef2a6109930494b74176
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-02-13 16:07:04 +0700

    percobaan 4

---
+++ MINGGU1/fungsi.java
@@ -0,0 +1,44 @@
+package MINGGU1;
+
+public class fungsi {
+
+    static int[] harga = {75000, 50000, 60000, 10000};
+
+public static int hitungPendapatan(int[] stok) {
+    int total = 0;
+    for (int i = 0; i < stok.length; i++) {
+        total += stok[i] * harga[i];
+    }
+    return total;
+  }
+
+  public static String statusCabang(int pendapatan){
+    if (pendapatan > 1500000) {
+        return "Sangat Baik";
+    } else {
+        return "Perlu Evaluasi";
+    }
+  }
+
+  public static void main(String[] args) {
+    int[][] stok = {
+        {10, 5, 15, 7},
+        {6, 11, 9, 12},
+        {2, 10, 10, 5},
+        {5, 7, 12, 9}
+    };
+
+    for (int i = 0; i < stok.length; i++) {
+        int pendapatan = hitungPendapatan(stok[i]);
+        String status = statusCabang(pendapatan);
+
+        System.out.println("RoyalGarden" + (i + 1));
+        System.out.println("Pendapatan : Rp " + pendapatan);
+        System.out.println("Status   : " + status);
+        System.out.println("----------------------------");
+    }
+  }
+
+
+}
+  
