4a55830575873085e4b9826a2ed50b4c0a9249a2
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-02-24 14:24:22 +0700

    tugas 2 minggu 2

--- MINGGU2/Dosen13.java
+++ MINGGU2/Dosen13.java
@@ -1,31 +1,29 @@
 package MINGGU2;
 
 public class Dosen13 {
-   String idDosen;
-   String nama;
-   boolean statusAktif;
-   int tahunBergabung;
-   String bidangKeahlian;
+    String idDosen;
+    String nama;
+    boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
 
-   void tampilkanInformasi() {
-    System.out.println("ID Dosen: " + idDosen);
-    System.out.println("Nama Dosen:" + nama);
-    System.out.println("Status aktif: " + statusAktif);
-    System.out.println("Tahun Bergabung: " + tahunBergabung);
-   }
+    void tampilkanInformasi() {
+        System.out.println("ID Dosen: " + idDosen);
+        System.out.println("Nama Dosen:" + nama);
+        System.out.println("Status Aktif: " + statusAktif);
+        System.out.println("Tahun Bergabung: " + tahunBergabung);
+    }
 
-   void setStatusAktif(boolean status) {
-    statusAktif = status;
-   }
+    void setStatusAktif(boolean status) {
+        statusAktif = status;
+    }
 
-   void hitungMasaKerja(int tahunSekarang) {
-    int masaKerja = tahunSekarang - tahunBergabung;
-      System.out.println("Masa Kerja: " + masaKerja + " tahun");
-   }
+    void hitungMasaKerja(int tahunSekarang) {
+        int masaKerja = tahunSekarang - tahunBergabung;
+        System.out.println("Masa Kerja: " + masaKerja + "tahun");
+    }
 
-   void ubahKeahlian(String keahlianBaru) {
-    System.out.println("Keahlian dosen " + nama + " diubah menjadi: " + keahlianBaru);
-   }
-
-    
+    void ubahKeahlian(String keahlianBaru) {
+        System.out.println("Keahlian Dosen :" + keahlianBaru);
+    }
 }
--- MINGGU2/DosenMain13.java
+++ MINGGU2/DosenMain13.java
@@ -3,17 +3,16 @@ package MINGGU2;
 public class DosenMain13 {
     public static void main(String[] args) {
         Dosen13 dosen1 = new Dosen13();
-        dosen1.idDosen = "D012";
-        dosen1.nama = "Dr. Ryu Ian";
+        dosen1.idDosen = "RI13";
+        dosen1.nama = "Ryu Ian";
         dosen1.statusAktif = true;
         dosen1.tahunBergabung = 2012;
-        dosen1.bidangKeahlian = "sastra Inggris";
+        dosen1.bidangKeahlian = "Sastra Rusia";
 
         dosen1.tampilkanInformasi();
         dosen1.setStatusAktif(false);
-        dosen1.hitungMasaKerja(2024);
-        dosen1.ubahKeahlian("sastra Jepang");
-
+        dosen1.hitungMasaKerja(2025);
+        dosen1.ubahKeahlian("Sastra Korea");
     }
     
 }
