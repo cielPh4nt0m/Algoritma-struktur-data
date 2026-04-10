46eb345264c41cb0333de15be6a22e84036bcf16
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-04-10 17:01:17 +0700

    Latihan prak 1

---
+++ MINGGU5/DataDosen13.java
@@ -0,0 +1,57 @@
+package MINGGU5;
+
+public class DataDosen13 {
+    public class DataDosen {
+    Dosen13[] dataDosen = new Dosen13[10];
+    int idx = 0;
+
+    public void tambah(Dosen13 dsn) {
+        if (idx < dataDosen.length) {
+            dataDosen[idx] = dsn;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!");
+        }
+    }  
+    public void tampil() {
+        for (int i = 0; i < idx; i++) {
+            dataDosen[i].tampil();
+        }
+    }
+    public void sortingASC() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 0; j < idx - i - 1; j++) {
+                if (dataDosen[j].usia > dataDosen[j + 1].usia) {
+                    Dosen13 temp = dataDosen[j];
+                    dataDosen[j] = dataDosen[j + 1];
+                    dataDosen[j + 1] = temp;
+            }
+            }
+        }
+    }
+    public void sortingDSC() {
+        for (int i = 0; i < idx - 1; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (dataDosen[j].usia > dataDosen[maxIdx].usia) {
+                    maxIdx = j;
+                }
+            }
+            Dosen13 temp = dataDosen[maxIdx];
+            dataDosen[maxIdx] = dataDosen[i];
+            dataDosen[i] = temp;
+        }
+    }
+    public void insertionSort() {
+        for (int i = 1; i < idx; i++) {
+            Dosen13 temp = dataDosen[i];
+            int j = i - 1;
+            while (j >= 0 && dataDosen[j].usia < temp.usia) {
+                dataDosen[j + 1] = dataDosen[j];
+                j--;
+            }
+            dataDosen[j + 1] = temp;
+        }
+    }
+}
+}
