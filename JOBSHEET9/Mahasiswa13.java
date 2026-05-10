7120e02da564fcc49c34797acda65c0f2df581bb
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-05-10 16:57:28 +0700

    Praktikum 1 & 2 (jb 9 )

---
+++ JOBSHEET9/Mahasiswa13.java
@@ -0,0 +1,16 @@
+package JOBSHEET9;
+
+public class Mahasiswa13 {
+    String nama, nim, kelas;
+    int nilai;
+    public Mahasiswa13(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
---
+++ JOBSHEET9/StackTugasMahasiswa13.java
@@ -0,0 +1,90 @@
+package JOBSHEET9;
+
+public class StackTugasMahasiswa13 {
+    Mahasiswa13[] stack;
+    int top;
+    int size;
+
+public StackTugasMahasiswa13(int size) {
+    this.size = size;
+    stack = new Mahasiswa13[size];
+    top = -1;
+ }
+
+public boolean isFull() {
+    if (top == size -1) {
+        return true;
+    } else {
+        return false;
+     }
+ }
+public boolean isEmpty() {
+    if (top == -1) {
+        return true;
+    } else {
+        return false;
+     }
+ }
+
+public void push(Mahasiswa13 mhs) {
+    if (!isFull()) {
+        top++;
+        stack[top] = mhs;
+    } else {
+        System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+    }
+ }
+public Mahasiswa13 pop() {
+    if (!isEmpty()) {
+        Mahasiswa13 mhs = stack[top];
+        top--;
+        return mhs;
+    } else {
+        System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+        return null;
+        }
+    }
+
+public Mahasiswa13 peek() {
+    if (!isEmpty()) {
+        return stack[top];
+    } else {
+        System.out.println("Stack kosong! Tidak ada tugas yang di kumpulkan.");
+        return null;    
+    }
+}
+
+public void peekBottom() {
+    if (!isEmpty()) {
+        System.out.println("Tugas Pertama dikumpulkan oleh: " + stack[0].nama);
+    } else {
+        System.out.println("Stack kosong!");
+    }
+}
+
+public int getCount() {
+    return top + 1;
+}
+
+public String konversiDesimalkeBiner(int nilai) {
+    StackKonversi13 stack = new StackKonversi13();
+    while (nilai != 0) {
+        int sisa = nilai % 2;
+        stack.push(sisa);
+        nilai = nilai / 2;
+    }
+    String biner = new String();
+    while (!stack.isEmpty()) {
+        biner += stack.pop();
+    }
+    return biner;
+}
+
+public void print() {
+    //for (int i = 0; i <= top; i++)
+     for (int i = top; i >=  0; i--) {
+        System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas );
+    }
+    System.out.println("");
+ }
+}
