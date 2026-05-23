bbb5dc014502835a82174cded31d246b16561dfb
cielPh4nt0m <sakamakimukamkitsukimaki@gmail.com>
2026-05-23 14:13:25 +0700

    P1Jobsheet10

---
+++ P1Jobsheet10/Queue13.java
@@ -0,0 +1,105 @@
+package P1Jobsheet10;
+
+public class Queue13 {
+int[] data;
+int front;
+int rear;
+int size;
+int max;
+
+public Queue13(int n) {
+    max = n;
+    data = new int[max];
+    size = 0;
+    front = rear = -1;
+ }
+
+ public boolean isEmpty() {
+    if (size == 0) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+ public boolean isFull() {
+    if (size == max) {
+        return true;
+    } else {
+        return false;
+    }
+ }
+
+ public void peek() {
+    if (!isEmpty()) {
+        System.out.println("Elemen terdepan: " + data[front]);
+    } else {
+        System.out.println("Queue masih kosong");
+    }
+ }
+
+ public void print() {
+    if (isEmpty()) {
+        System.out.println("Queue masih kosong");
+    } else {
+        int i = front;
+        System.out.print(data[i] + " ");
+        i = (i + 1) % max;
+    }
+    System.out.println(data[i] + " ");
+    System.out.println("Jumlah elemen = " + size);
+ }
+
+ public void clear() {
+    if (!isEmpty()) {
+        front = rear = -1;
+        size = 0;
+        System.out.println("Queue berhasil dikosongkan");
+    } else {
+        System.out.println("Queue masih kosong");
+    }
+ }
+
+ public void  Enqueue(int dt) {
+    if (isFull()) {
+        System.out.println("Queue sudah penuh");
+        System.out.println("Program dihentikan secara otomatis!");
+        System.exit(0); // menghentikan program
+    } else {
+        if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            if (rear == max -1) {
+                rear = 0;
+            } else {
+                rear++;
+            }
+        }
+        data[rear] = dt;
+        size++;
+    }
+ }
+
+ public int Dequeue() {
+    int dt = 0;
+    if (isEmpty()) {
+        System.out.println("Queue masih kosong");
+        System.out.println("Program dihentikan secara otomatis!");
+        System.exit(0); // menghentikan program
+    } else {
+        dt = data[front];
+        size--;
+        if (isEmpty()) {
+            front = rear = -1;
+        } else {
+            if (front == max -1) {
+                front = 0;
+            } else {
+                front++;
+            }
+        }
+    }
+    return dt;
+ }
+}
+
